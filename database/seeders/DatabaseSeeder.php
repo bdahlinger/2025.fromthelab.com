b59c300103fe34e40dc5bf3dfbb1912549514bf1
Bill Dahlinger <bill@platform.la>
2025-02-28 20:09:14 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Bill Dahlinger',
+            'email' => 'bill@fromthelab.com',
+        ]);
+        User::factory()->create([
+            'name' => 'James Allen',
+            'email' => 'james@shinyrobot.io',
         ]);
     }
 }
